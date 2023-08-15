f55c49527c3f8186ea22c6d80677916afbcc02f0
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-08-15 14:06:39 +0200

    Imp: Relocating and exporting hook types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,4 +3,5 @@ export * from './attributes';
 export * from './events';
 export * from './elements';
 export * from './styles';
+export * from './hooks';
 // TODO: Import specifics from events (abstraction)
