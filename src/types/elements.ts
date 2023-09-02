672937e7d54b01238f56714d0ac6ee7eabf9607c
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-09-02 21:56:07 +0200

    Feat: Added all SVG Elements

--- src/types/elements.ts
+++ src/types/elements.ts
@@ -20,7 +20,8 @@ import {
     TdHTMLAttributes, 
     TextareaHTMLAttributes, 
     ThHTMLAttributes, 
-    VideoHTMLAttributes
+    VideoHTMLAttributes,
+    SVGAttributes
 } from './attributes';
 
 export interface KiwuiHTML {
@@ -68,4 +69,65 @@ export interface KiwuiHTML {
     title: JSXElement<HTMLAttributes, HTMLTitleElement>
     ul: JSXElement<HTMLAttributes, HTMLUListElement>
     video: JSXElement<VideoHTMLAttributes, HTMLVideoElement>
+
+    // SVG
+    svg: SVGAttributes<SVGSVGElement>;
+
+    animate: SVGAttributes<SVGElement>; // TODO: It is SVGAnimateElement but is not in TypeScript's lib.dom.d.ts for now.
+    animateMotion: SVGAttributes<SVGElement>;
+    animateTransform: SVGAttributes<SVGElement>; // TODO: It is SVGAnimateTransformElement but is not in TypeScript's lib.dom.d.ts for now.
+    circle: SVGAttributes<SVGCircleElement>;
+    clipPath: SVGAttributes<SVGClipPathElement>;
+    defs: SVGAttributes<SVGDefsElement>;
+    desc: SVGAttributes<SVGDescElement>;
+    ellipse: SVGAttributes<SVGEllipseElement>;
+    feBlend: SVGAttributes<SVGFEBlendElement>;
+    feColorMatrix: SVGAttributes<SVGFEColorMatrixElement>;
+    feComponentTransfer: SVGAttributes<SVGFEComponentTransferElement>;
+    feComposite: SVGAttributes<SVGFECompositeElement>;
+    feConvolveMatrix: SVGAttributes<SVGFEConvolveMatrixElement>;
+    feDiffuseLighting: SVGAttributes<SVGFEDiffuseLightingElement>;
+    feDisplacementMap: SVGAttributes<SVGFEDisplacementMapElement>;
+    feDistantLight: SVGAttributes<SVGFEDistantLightElement>;
+    feDropShadow: SVGAttributes<SVGFEDropShadowElement>;
+    feFlood: SVGAttributes<SVGFEFloodElement>;
+    feFuncA: SVGAttributes<SVGFEFuncAElement>;
+    feFuncB: SVGAttributes<SVGFEFuncBElement>;
+    feFuncG: SVGAttributes<SVGFEFuncGElement>;
+    feFuncR: SVGAttributes<SVGFEFuncRElement>;
+    feGaussianBlur: SVGAttributes<SVGFEGaussianBlurElement>;
+    feImage: SVGAttributes<SVGFEImageElement>;
+    feMerge: SVGAttributes<SVGFEMergeElement>;
+    feMergeNode: SVGAttributes<SVGFEMergeNodeElement>;
+    feMorphology: SVGAttributes<SVGFEMorphologyElement>;
+    feOffset: SVGAttributes<SVGFEOffsetElement>;
+    fePointLight: SVGAttributes<SVGFEPointLightElement>;
+    feSpecularLighting: SVGAttributes<SVGFESpecularLightingElement>;
+    feSpotLight: SVGAttributes<SVGFESpotLightElement>;
+    feTile: SVGAttributes<SVGFETileElement>;
+    feTurbulence: SVGAttributes<SVGFETurbulenceElement>;
+    filter: SVGAttributes<SVGFilterElement>;
+    foreignObject: SVGAttributes<SVGForeignObjectElement>;
+    g: SVGAttributes<SVGGElement>;
+    image: SVGAttributes<SVGImageElement>;
+    line: SVGAttributes<SVGLineElement>;
+    linearGradient: SVGAttributes<SVGLinearGradientElement>;
+    marker: SVGAttributes<SVGMarkerElement>;
+    mask: SVGAttributes<SVGMaskElement>;
+    metadata: SVGAttributes<SVGMetadataElement>;
+    mpath: SVGAttributes<SVGElement>;
+    path: SVGAttributes<SVGPathElement>;
+    pattern: SVGAttributes<SVGPatternElement>;
+    polygon: SVGAttributes<SVGPolygonElement>;
+    polyline: SVGAttributes<SVGPolylineElement>;
+    radialGradient: SVGAttributes<SVGRadialGradientElement>;
+    rect: SVGAttributes<SVGRectElement>;
+    stop: SVGAttributes<SVGStopElement>;
+    switch: SVGAttributes<SVGSwitchElement>;
+    symbol: SVGAttributes<SVGSymbolElement>;
+    text: SVGAttributes<SVGTextElement>;
+    textPath: SVGAttributes<SVGTextPathElement>;
+    tspan: SVGAttributes<SVGTSpanElement>;
+    use: SVGAttributes<SVGUseElement>;
+    view: SVGAttributes<SVGViewElement>;
 }
