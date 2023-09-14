7b58942c953299269ec70a78592da95f624f795e
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-09-14 20:00:49 +0200

    Feat: createContext

---
+++ src/core/createContext/createContext.ts
@@ -0,0 +1,15 @@
+// import { ContextConsumer } from "../../types";
+
+import { Context } from "../../types"
+
+export const createContext = <T>(initialValue: T): Context<T> => {
+    const Provider: Context<T>['Provider'] = ({ children }) => 
+        Array.isArray(children) ? children[0] : children; // TODO: Handle fragments
+
+    Provider.exoticTag = 'Context';
+
+    return {
+        initialValue,
+        Provider
+    }
+}
