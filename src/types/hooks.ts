b07b1c17cab4e6a8401bec6891476c153c1ec9bf
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-10-01 12:26:07 +0200

    Feat: Allowing ref in HTML Elements

--- src/types/hooks.ts
+++ src/types/hooks.ts
@@ -7,7 +7,7 @@ export type Hooks = {
     useLayoutEffect(effect: Effect, dependencies?: Dependencies): void
     useMemo<T>(getter: () => T, dependencies: Dependencies): T
     useCallback<T extends Function>(callback: T, dependencies: Dependencies): T
-    useRef<T>(initialValue: T): Reference<T>
+    useRef<T>(initialValue: T): ReferenceObject<T>
     useContext<T>(context: Context<T>): T
 }
 
@@ -21,9 +21,13 @@ export type Cleanup = () => void;
 export type Dependencies = ReadonlyArray<unknown>;
 
 // Reference
-export type Reference<T> = {
+export type Reference<T> = ReferenceObject<T> | ReferenceCallback<T>
+
+export type ReferenceObject<T> = {
     current: T
 }
+
+export type ReferenceCallback<T> = (instance: T | null) => void
 
 // Context
 export type Context<T> = {
