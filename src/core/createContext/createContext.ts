48bf546046d907446107cf1ab46a2428ecfe6044
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-11-16 11:13:34 +0100

    Fix: Fragments for contexts

--- src/core/createContext/createContext.ts
+++ src/core/createContext/createContext.ts
@@ -1,9 +1,7 @@
 import { Context } from "../../types"
 
 export const createContext = <T>(initialValue: T): Context<T> => {
-    const Provider: Context<T>['Provider'] = ({ children }) => 
-        Array.isArray(children) ? children[0] : children; // TODO: Handle fragments
-
+    const Provider: Context<T>['Provider'] = ({ children }) => children;
     Provider.exoticTag = 'Context';
 
     return {
